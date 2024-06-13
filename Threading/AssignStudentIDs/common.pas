1bec8a97957529d9482ef51719bbd82c66cdbbbd
Iwan Kelaiah <iwan.kelaiah@gmail.com>
2024-06-13 20:53:50 +1000

    Update - Removed LGenerics from project.

--- Threading/AssignStudentIDs/common.pas
+++ Threading/AssignStudentIDs/common.pas
@@ -5,7 +5,7 @@ unit Common;
 interface
 
 uses
-  Classes, SysUtils, lgVector;
+  Classes, SysUtils, Generics.Defaults, Generics.Collections;
 
 type
   TStudent = record
@@ -14,16 +14,32 @@ type
   end;
 
 type
-  TStudentList = specialize TGLiteVector<TStudent>;
-  TStrList = specialize TGLiteVector<string>;
+  TStudentList = specialize TList<TStudent>;
+  TStudentListComparer = specialize TComparer<TStudent>;
+
+type
+  TStrList = specialize TList<string>;
 
 const
   maxThreads: int64 = 4;
 
 var
+  // start of student ID to assign
   startStudentID: int64 = 200000;
+
+  // A list of TStudent records
   finalStudentList: TStudentList;
 
+// Custom comparison function for sorting by name - ascending
+function CompareName(const LeftItem, RightItem: TStudent): integer;
+
 implementation
+
+// Custom comparison function for sorting by name - ascending
+function CompareName(const LeftItem, RightItem: TStudent): integer;
+begin
+  Result := CompareStr(LeftItem.Name, RightItem.Name);
+end;
+
 
 end.
